a924986e423c5fd966a374048552361ec9e0b38b
Jorge M.G <jorge@on-orbit.dev>
2021-05-07 16:20:31 +0200

    New izzo2015 solver

--- src/lamberthub/__init__.py
+++ src/lamberthub/__init__.py
@@ -1,17 +1,18 @@
 """ A collection of Lambert's problem solvers """
 
 from lamberthub.universal_solvers.gooding import gooding1990
+from lamberthub.universal_solvers.izzo import izzo2015
 
 __version__ = "0.1.dev0"
 
-ALL_SOLVERS = [gooding1990]
+ALL_SOLVERS = [gooding1990, izzo2015]
 """ A list holding all lamberthub available solvers """
 
-ZERO_REV_SOLVERS = [gooding1990]
+ZERO_REV_SOLVERS = [gooding1990, izzo2015]
 """ A list holding all direct-revolution lamberthub solvers """
 
-MULTI_REV_SOLVERS = [gooding1990]
+MULTI_REV_SOLVERS = [gooding1990, izzo2015]
 """ A list holding all multi-revolution lamberthub solvers """
 
-ROBUST_SOLVERS = [gooding1990]
+ROBUST_SOLVERS = [gooding1990, izzo2015]
 """ A list holding all robust lamberthub solvers """
