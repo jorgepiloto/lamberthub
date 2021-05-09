ae0691ce6756251b6286d9ea22954feba0d5f642
Jorge M.G <jorge@on-orbit.dev>
2021-05-09 10:16:54 +0200

    Include more orbit angles utilities

--- src/lamberthub/utils/angles.py
+++ src/lamberthub/utils/angles.py
@@ -38,8 +38,8 @@ def get_transfer_angle(r1, r2, prograde):
     alpha = dot(np.array([0, 0, 1]), h)
 
     # Get the minimum angle (0 <= dtheta <= pi) between r1 and r2.
-    r1_norm, r2_norm = [norm(vec) for vec in [r1, r2]]
-    theta0 = np.arccos(dot(r1, r2) / (r1_norm * r2_norm))
+    rnorm, r2_norm = [norm(vec) for vec in [r1, r2]]
+    theta0 = np.arccos(dot(r1, r2) / (rnorm * r2_norm))
 
     # Fix the value of theta if necessary
     if prograde is True:
@@ -86,3 +86,185 @@ def get_orbit_normal_vector(r1, r2, prograde):
         i_h = i_h if alpha < 0 else -i_h
 
     return i_h
+
+
+def get_orbit_inc_and_raan_from_position_vectors(r1, r2, prograde):
+    """
+    Computes the inclination of the orbit being known an initial and a final
+    position vectors together with the sense of motion.
+
+    Parameters
+    ----------
+    r1: np.array
+        Initial position vector.
+    r2: np.array
+        Final position vector.
+    prograde: bool
+        If True, it assumes prograde motion, otherwise assumes retrograde.
+
+    Returns
+    -------
+    inc: float
+        Inclination of the orbit.
+    raan: float
+        Right ascension of the ascending node.
+
+    """
+
+    # Get a unitary vector aligned in direction and sense with the specific
+    # angular momentum one.
+    i_h = get_orbit_normal_vector(r1, r2, prograde)
+
+    # Define the unitary vector along Z-axis of the fundamental plane
+    i_K = np.array([0, 0, 1])
+
+    # If the orbit is coplanar with fundamental plane, neither inc or raan are
+    # defined. TODO: use atol and rtol instead of pure floating zero comparison
+    if i_h[0] == 0 and i_h[1] == 0:
+        inc, raan = 0, 0
+    else:
+        # Inclination is always bounded between [0, pi], so no correction is
+        # needed
+        inc = np.arccos(i_h[2] / norm(i_h))
+
+        # Compute the RAAN using a vector in the direction and sense of the line
+        # of nodes. Because RAAN is bounded between [0, 2pi], the arctan2
+        # function is used.
+        n = cross(i_K, i_h)
+        raan = np.arctan2(n[1], n[0]) % (2 * np.pi)
+
+    return inc, raan
+
+
+def nu_to_E(nu, ecc):
+    """
+    Retrieves eccentric anomaly from true one.
+
+    Parameters
+    ----------
+    nu: float
+        True anomaly.
+    ecc: float
+        Eccentricity of the orbit.
+
+    Returns
+    -------
+    E: float
+        Eccentric anomaly.
+
+    """
+
+    E = 2 * np.arctan(np.sqrt((1 - ecc) / (1 + ecc)) * np.tan(nu / 2))
+    return E
+
+
+def E_to_nu(E, ecc):
+    """
+    Retrieves true anomaly from eccentric one.
+
+    Parameters
+    ----------
+    E: float
+        Eccentric anomaly.
+    ecc: float
+        Eccentricity of the orbit.
+
+    Returns
+    -------
+    nu: float
+        True anomaly.
+
+    """
+
+    nu = 2 * np.arctan(np.sqrt((1 + ecc) / (1 - ecc)) * np.tan(E / 2))
+    return nu
+
+
+def nu_to_B(nu):
+    """
+    Retrieves parabolic anomaly from true one.
+
+    Parameters
+    ----------
+    nu: float
+        True anomaly
+
+    Returns
+    -------
+    B: float
+        Parabolic anomaly
+
+    Notes
+    -----
+    As explained in Vallado's [1], :math:`B` is used instead of :math:`P` just
+    to not confuse with the orbital parameter.
+
+    """
+    B = np.tan(nu / 2)
+    return B
+
+
+def B_to_nu(B):
+    """
+    Retrieves the true anomaly from parabolic one.
+
+    Parameters
+    ----------
+    B: float
+        Parabolic anomaly
+
+    Returns
+    -------
+    nu: float
+        True anomaly
+
+    Notes
+    -----
+    As explained in Vallado's [1], :math:`B` is used instead of :math:`P` just
+    to not confuse with the orbital parameter.
+
+    """
+    nu = 2 * np.arctan(B)
+    return nu
+
+
+def nu_to_H(nu, ecc):
+    """
+    Retrieves hyperbolic anomaly from true one.
+
+    Parameters
+    ----------
+    nu: float
+        True anomaly
+    ecc: float
+        Eccentricity of the orbit
+
+    Returns
+    -------
+    H: float
+        Hyperbolic anomaly
+
+    """
+    H = 2 * np.arctanh(np.sqrt((ecc - 1) / (ecc + 1)) * np.tan(nu / 2))
+    return H
+
+
+def H_to_nu(H, ecc):
+    """
+    Retrieves hyperbolic anomaly from true one.
+
+    Parameters
+    ----------
+    H: float
+        Hyperbolic anomaly
+    ecc: float
+        Eccentricity of the orbit
+
+    Returns
+    -------
+    nu: float
+        True anomaly
+
+    """
+    nu = 2 * np.arctan(np.sqrt((ecc + 1) / (ecc - 1)) * np.tanh(H / 2))
+    return nu
