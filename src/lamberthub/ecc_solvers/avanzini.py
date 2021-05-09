5c2097d9203642bd50f4d3239284f595638647f8
Jorge M.G <jorge@on-orbit.dev>
2021-05-09 10:17:19 +0200

    New avanzini2008 solver

---
+++ src/lamberthub/ecc_solvers/avanzini.py
@@ -0,0 +1,204 @@
+"""
+Lambert's problem solver using the method proposed by Giulio Avanzini in 2008.
+
+This module holds the implementation of the algorithm devised by Avanzini, which
+appeared for the first time in his article [1]. The solver takes advantage of
+the conservation of the eccentricity projection along the chord direction, so it
+iterates over this orbital element till the time of transfer from Kepler's
+equation matches the desired transfer one.
+
+However, the original routines was not developed to work under multi-revolutions
+conditions. In addition, Avanzini did not provide the derivative of Kepler's
+equation with respect to the transverse eccentricity component, forcing a
+bounded numerical method (bisection or regulafalsi). This was solved by Quan He,
+which proposed new changes to Avanzini's original algorithm in an article [2]
+proposed in 2010.
+
+Finally, a new method [3] was proposed by Changxuan Wen in 2014, simplifying all
+previous routines.
+
+[1] Avanzini, G. (2008). A simple Lambert algorithm. Journal of guidance,
+    control, and dynamics, 31(6), 1587-1594.
+
+[2] He, Q., Li, J., & Han, C. (2010). Multiple-revolution solutions of the
+    transverse-eccentricity-based lambert problem. Journal of guidance,
+    control, and dynamics, 33(1), 265-269.
+
+[3] Wen, C., Zhao, Y., & Shi, P. (2014). Derivative analysis and algorithm
+    modification of transverse-eccentricity-based Lambert problem. Journal of
+    Guidance, Control, and Dynamics, 37(4), 1195-1201.
+
+"""
+
+import numpy as np
+from scipy.optimize import newton
+
+from lamberthub.ecc_solvers.utils import (
+    _f,
+    coe_at_eccT,
+    get_fundamental_ellipse_properties,
+    get_geometry,
+)
+from lamberthub.utils.assertions import assert_parameters_are_valid
+from lamberthub.utils.elements import coe2rv
+
+
+def avanzini2008(
+    mu,
+    r1,
+    r2,
+    tof,
+    M=0,
+    prograde=True,
+    low_path=True,
+    maxiter=35,
+    atol=1e-5,
+    rtol=1e-7,
+    full_output=False,
+):
+    r"""
+    Solves Lambert problem using Gooding's devised algorithm.
+
+    Parameters
+    ----------
+    mu: float
+        Gravitational parameter, equivalent to :math:`GM` of attractor body.
+    r1: numpy.array
+        Initial position vector.
+    r2: numpy.array
+        Final position vector.
+    M: int
+        Number of revolutions. Must be equal or greater than 0 value.
+    prograde: bool
+        If `True`, specifies prograde motion. Otherwise, retrograde motion is imposed.
+    low_path: bool
+        If two solutions are available, it selects between high or low path.
+    maxiter: int
+        Maximum number of iterations.
+    atol: float
+        Absolute tolerance.
+    rtol: float
+        Relative tolerance.
+    full_output: bool
+        If True, the number of iterations is also returned.
+
+    Returns
+    -------
+    v1: numpy.array
+        Initial velocity vector.
+    v2: numpy.array
+        Final velocity vector.
+    numiter: list
+        Number of iterations.
+
+    Notes
+    -----
+    The following routine might be simplified making use of private functions.
+    However, we decided to expose all the auxiliary routines to properly
+    reproduce original report figures.
+
+    """
+
+    # Check proper value of multi-revolution. Although we could not accept M at
+    # all, this ensures all routines within the library work under the same
+    # number and type of parameters.
+    if M > 0:
+        raise ValueError(
+            "Avanzini is not able to work within the multi-revolution scenario!"
+        )
+
+    # Check that input parameters are safe
+    assert_parameters_are_valid(mu, r1, r2, tof, M)
+
+    # Start by computing an auxiliary set of geometric parameters
+    geometry = get_geometry(r1, r2, prograde)
+
+    # Filter out the evolution of ecc_T w.r.t. the independent variable based on
+    # the transfer angle criteria, see original report flowchart.
+    eccT_at_x = _get_eccT_at_x(geometry)
+
+    # Iterative process based on Newton-Raphson secant method begins. Avanzini
+    # did not provided a derivative for Kepler's equation of time w.r.t. the ecc_T
+    # variable, so this root solver is the one required rather than the pure N-R
+    # one.
+    x_sol, r = newton(
+        _f,
+        0,
+        args=(eccT_at_x, mu, geometry, tof),
+        maxiter=maxiter,
+        tol=atol,
+        rtol=rtol,
+        full_output=True,
+    )
+
+    # Solve the actual value of ecc_T at solved x and retrieve COE elements
+    ecc_T = eccT_at_x(x_sol)
+    p, ecc, inc, raan, argp, nu_1, nu_2 = coe_at_eccT(ecc_T, r1, r2, prograde)
+
+    # Compute the velocity vectors from the classic orbital elements
+    (_, v1), (_, v2) = [coe2rv(mu, p, ecc, inc, raan, argp, nu) for nu in [nu_1, nu_2]]
+
+    # Extract the number of iterations
+    numiter = r.iterations
+
+    return (v1, v2, numiter) if full_output is True else (v1, v2)
+
+
+def _get_eccT_limits(geometry):
+    """
+    Computes transverse eccentricity value limits as of problem geometry.
+    """
+
+    # Solve for the fundamental ellipse properties
+    r1_norm, r2_norm, c_norm, dtheta, _ = geometry
+    ecc_F, a_F, p_F = get_fundamental_ellipse_properties(r1_norm, r2_norm, c_norm)
+
+    # Compute the limits
+    ecc_max = -1 / np.cos(dtheta / 2)
+    ecc_H = np.sqrt(ecc_max ** 2 - ecc_F ** 2) if dtheta > np.pi else np.inf
+    ecc_P = np.sqrt(1 - ecc_F ** 2)
+
+    return ecc_H, ecc_P
+
+
+def _get_eccT_at_x(geometry):
+    """
+    Returns proper transverse eccentricity function depending on the value
+    of the transfer angle.
+
+    Parameters
+    ----------
+    ecc_H: float
+        Lower value limit.
+    ecc_P: float
+        Upper value limit.
+    dtheta: float
+        Transfer angle.
+
+    Returns
+    -------
+    eccT_at_x: function
+        A function to be evaluated at independent variable values.
+
+    Notes
+    -----
+    These are equations (16) and (18) from the official report [1].
+
+    """
+
+    # Compute the limits of the ecc_T value
+    ecc_H, ecc_P = _get_eccT_limits(geometry)
+    _, _, _, dtheta, _ = geometry
+
+    if dtheta > np.pi:
+        # Equation (16) is applied
+        def eccT_at_x(x):
+            X = np.exp((1 / ecc_H + 1 / ecc_P) * x)
+            return ecc_P * ecc_H * (X - 1) / (ecc_P + ecc_H * X)
+
+    else:
+        # Equation (18) is applied
+        def eccT_at_x(x):
+            return ecc_P * (1 - np.exp(-x / ecc_P))
+
+    return eccT_at_x
---
+++ src/lamberthub/ecc_solvers/utils.py
@@ -0,0 +1,517 @@
+"""
+Holds auxiliary functions used by eccentricity based Lambert's problem
+solvers. The majority of the routines hosted within this module were directly
+taken from the following publications [1] [2] [3].
+
+[1] Avanzini, G. (2008). A simple Lambert algorithm. Journal of guidance,
+    control, and dynamics, 31(6), 1587-1594.
+
+[2] He, Q., Li, J., & Han, C. (2010). Multiple-revolution solutions of the
+    transverse-eccentricity-based Lambert problem. Journal of guidance,
+    control, and dynamics, 33(1), 265-269.
+
+[3] Wen, Changxuan, Yushan Zhao, and Peng Shi. "Derivative analysis and
+    algorithm modification of transverse-eccentricity-based Lambert problem."
+    Journal of Guidance, Control, and Dynamics 37.4 (2014): 1195-1201.
+
+"""
+
+import numpy as np
+from numpy import cross
+from numpy.linalg import norm
+
+from lamberthub.utils.angles import (
+    get_orbit_inc_and_raan_from_position_vectors,
+    get_orbit_normal_vector,
+    get_transfer_angle,
+)
+from lamberthub.utils.assertions import assert_transfer_angle_not_zero
+from lamberthub.utils.kepler import kepler_from_nu
+
+
+def get_geometry(r1, r2, prograde):
+    """
+    Computes associated problem geometry.
+
+    Parameters
+    -----------
+    r1: np.array
+        Initial position vector.
+    r2: np.array
+        Final position vector.
+    prograde: bool
+        If True, assumes prograde motion, otherwise retrograde is applied.
+
+    Returns
+    -------
+    r1_norm: float
+        Norm of the initial position vector.
+    r1_norm: float
+        Norm of the final position vector.
+    c_norm: float
+        Norm of the chord vector.
+    dtheta: float
+        Transfer angle.
+    w_c: float
+        Angle between initial position and chord vectors.
+
+    """
+
+    # Solve for the norms
+    r1_norm, r2_norm, c_norm = [np.linalg.norm(vec) for vec in [r1, r2, (r2 - r1)]]
+
+    # Compute angles
+    dtheta = get_transfer_angle(r1, r2, prograde)
+    assert_transfer_angle_not_zero(dtheta)
+    w_c = get_transfer_angle(r1, (r2 - r1), prograde)
+
+    return r1_norm, r2_norm, c_norm, dtheta, w_c
+
+
+def get_eccF(r1_norm, r2_norm, c_norm):
+    """
+    Computes the eccentricity component along the chord. This value is kept
+    constant for all the problem as long as the boundary conditons are not
+    changed.
+
+    Parameters
+    ----------
+    r1_norm: float
+        Norm of the initial vector position.
+    r2_norm: float
+        Norm of the final vector position.
+    c_norm: float
+        Norm of the chord vector.
+
+    Returns
+    -------
+    ecc_F: float
+        Eccentricity component along the chord direction.
+
+    Notes
+    -----
+    Equation (3) from Avanzini's report [1].
+
+    """
+    ecc_F = (r1_norm - r2_norm) / c_norm
+    return ecc_F
+
+
+def get_aF(r1_norm, r2_norm):
+    """
+    Computes the semi-major axis of the fundamental ellipse. This value is
+    kept constant for all the problem as long as the boundary conditions are not
+    changed.
+
+    Parameters
+    ----------
+    r1_norm: float
+        Norm of the initial vector position.
+    r2_norm: float
+        Norm of the final vector position.
+
+    Returns
+    -------
+    a_F: float
+        Semi-major axis of the fundamental ellipse.
+
+    Notes
+    -----
+    No labeled equation (appears between [3] and [4]) from Avanzini's report
+    [1].
+
+    """
+
+    a_F = (r1_norm + r2_norm) / 2
+    return a_F
+
+
+def get_pF(a_F, ecc_F):
+    """
+    Computes the orbital parameter (semi-latus) rectum of the fundamental
+    ellipse. This value is kept constant for all the problem as long as the
+    boundary conditions are not changed.
+
+    Parameters
+    ----------
+    a_F: float
+        Semi-major axis of the fundamental ellipse.
+    ecc_F: float
+        Eccentricity of the fundamental ellipse.
+
+    Returns
+    -------
+    p_F: float
+        Orbital parameter / semi-latus rectum of the fundamental ellipse.
+
+    Notes
+    -----
+    No labeled equation (appears between [3] and [4]) from Avanzini's report
+
+    """
+    p_F = a_F * (1 - ecc_F ** 2)
+    return p_F
+
+
+def get_fundamental_ellipse_properties(r1_norm, r2_norm, c_norm):
+    """
+    Computes the fundamental ellipse properties. Those are the eccentricity,
+    semi-major axis and the orbital parameter.
+
+    Parameters
+    ----------
+    r1_norm: float
+        Norm of the initial vector position.
+    r2_norm: float
+        Norm of the final vector position.
+    c_norm: float
+        Norm of the chord vector.
+
+    Returns
+    -------
+    ecc_F: float
+        Eccentricity component along the chord direction.
+    a_F: float
+        Semi-major axis of the fundamental ellipse.
+    p_F: float
+        Orbital parameter / semi-latus rectum of the fundamental ellipse.
+
+    """
+
+    # Compute the fundamental ellipse parameters
+    ecc_F = get_eccF(r1_norm, r2_norm, c_norm)
+    a_F = get_aF(r1_norm, r2_norm)
+    p_F = get_pF(a_F, ecc_F)
+
+    return ecc_F, a_F, p_F
+
+
+def ecc_at_eccT(ecc_T, ecc_F):
+    """
+    Computes transfer orbit eccentricity from transverse and fundamental
+    components.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Eccentricity transverse component.
+    ecc_F: float
+        Eccentricity of the fundamental ellipse.
+
+    Returns
+    -------
+    ecc: float
+        Eccentricity of the transfer orbit.
+
+    """
+
+    ecc = np.sqrt(ecc_T ** 2 + ecc_F ** 2)
+    return ecc
+
+
+def p_at_eccT(ecc_T, r1_norm, r2_norm, c_norm, dtheta, p_F):
+    """
+    Computes the orbital parameter or semi-latus rectum of the transfer orbit.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Eccentricity transverse component.
+    r1_norm: float
+        Norm of the initial vector position.
+    r2_norm: float
+        Norm of the final vector position.
+    c_norm: float
+        Norm of the chord vector.
+    dtheta: float
+        Transfer angle.
+    p_F: float
+        Orbital parameter or semi-latus rectum of the fundamental ellipse.
+
+    Returns
+    -------
+    p: float
+        Orbital parameter or semi-lactus rectum.
+
+    """
+    p = p_F - ecc_T * r1_norm * r2_norm * np.sin(dtheta) / c_norm
+    return p
+
+
+def a_at_eccT(ecc_T, ecc_F, p):
+    """
+    Computes the semi-major axis of the transfer orbit.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Eccentricity transverse component.
+    ecc_F: float
+        Eccentricity of the fundamental ellipse.
+    p: float
+        Transfer orbit parameter or semi-latus rectum.
+
+    Returns
+    -------
+    a: float
+        Semi-major axis of the transfer orbit.
+
+    """
+    a = p / (1 - ecc_F ** 2 - ecc_T ** 2)
+    return a
+
+
+def eap_from_eccT(ecc_T, geometry):
+    """
+    Solves for transfer orbit eccentricity, semi-major axis and orbital
+    parameter.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Eccentricity component along transverse direction.
+
+    geometry: tuple
+        A tuple hosting r1_norm, r2_norm, c_norm, dtheta and w_c geometry values.
+
+    Returns
+    -------
+    ecc: float
+        Absolute eccentricity of the transfer orbit.
+    a: float
+        Semi-major axis of the transfer orbit.
+    p: float
+        Semi-latus rectum of the transfer orbit.
+
+    """
+
+    # Unpack useful parameters
+    r1_norm, r2_norm, c_norm, dtheta, w_c = geometry
+
+    # Solve for the fundamental ellipse properties
+    ecc_F, a_F, p_F = get_fundamental_ellipse_properties(r1_norm, r2_norm, c_norm)
+
+    # Compute the transfer orbit eccentricity, semi-latus rectum and
+    # semi-major axis.
+    ecc = ecc_at_eccT(ecc_T, ecc_F)
+    p = p_at_eccT(ecc_T, r1_norm, r2_norm, c_norm, dtheta, p_F)
+    a = a_at_eccT(ecc_T, ecc_F, p)
+
+    return ecc, a, p
+
+
+def w_at_eccT(ecc_T, ecc_F, w_c):
+    """
+    Compute the true anomalies for the initial and final position vectors
+    with respect to the transfer orbit.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Eccentricity transverse component.
+    ecc_F: float
+        Eccentricity of the fundamental ellipse.
+    dtheta: float
+        Transfer angle.
+    w_c: float
+        Angle between the initial and chord vector.
+
+    Returns
+    -------
+    nu_1: float
+        True anomaly of the initial position vector w.r.t. transfer orbit.
+    nu_2: float
+        True anomaly of the final position vector w.r.t. transfer orbit.
+
+    Notes
+    -----
+    This is equation (6) from Quan He's report [2].
+
+    """
+
+    # Compute the coordinates
+    y = ecc_F * np.sin(w_c) + ecc_T * np.cos(w_c)
+    x = ecc_F * np.cos(w_c) - ecc_T * np.sin(w_c)
+    w = np.arctan2(y, x)
+
+    return w
+
+
+def get_true_anomalies(w, dtheta):
+    """
+    Compute the initial and final true anomalies.
+
+    Parameters
+    ----------
+    w: float
+        Argument of periapsis.
+    dtheta: float
+        Transfer angle.
+
+    Returns
+    -------
+    nu_1: float
+        Initial true anomaly.
+    nu_2: float
+        Final true anomaly.
+
+    """
+    nu_1 = -w
+    nu_2 = nu_1 + dtheta
+    return nu_1, nu_2
+
+
+def kepler_tof_at_eccT(ecc_T, mu, geometry):
+    """
+    Computes the time of flight at particular value of transverse eccentricity
+    and problem boundary conditions.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Eccentricity component along transverse direction.
+    mu: float
+        The gravitational parameter.
+    geometry: tuple
+        A tuple hosting r1_norm, r2_norm, c_norm, dtheta and w_c geometry values.
+
+    Returns
+    -------
+    tof: float
+        Dimensional time of flight from Kepler's equation.
+
+    """
+
+    # Unpack useful parameters
+    r1_norm, r2_norm, c_norm, dtheta, w_c = geometry
+
+    # Compute the transfer orbit eccentricity, semi-latus rectum and
+    # semi-major axis.
+    ecc, a, p = eap_from_eccT(ecc_T, geometry)
+    ecc_F, _, _ = get_fundamental_ellipse_properties(r1_norm, r2_norm, c_norm)
+
+    # Last step before solving Kepler's equation is to compute the true
+    # anomalies. Hence, the argument of periapsis is required to do so.
+    w = w_at_eccT(ecc_T, ecc_F, w_c)
+    nu_1, nu_2 = get_true_anomalies(w, dtheta)
+
+    # Compute the change in mean motion from corresponding Kepler's
+    # equation, that is elliptical, parabolic or hyperbolic.
+    M_1, M_2 = [kepler_from_nu(nu, ecc) for nu in [nu_1, nu_2]]
+    deltaM = M_2 - M_1
+
+    # Compute the time of flight for that particular value of mean motion
+    # variation.
+    if 0 <= ecc < 1:
+        tof12 = np.sqrt(a ** 3 / mu) * deltaM
+        if tof12 < 0:
+            # Add an orbital period so the time of flight becomes positive
+            tof12 += 2 * np.pi * np.sqrt(a ** 3 / mu)
+    elif ecc == 1:
+        tof12 = (1 / 2) * np.sqrt(p ** 3 / mu) * deltaM
+        if tof12 < 0:
+            return np.inf
+    else:
+        tof12 = np.sqrt(-(a ** 3) / mu) * deltaM
+
+    return tof12
+
+
+def _f(x, ecc_T_at_x, mu, geometry, tof12_s):
+    """
+    Returns a zero once the value of x makes the numerically compute time of
+    flight to be exactly the desired one.
+
+    Parameters
+    ----------
+    x: float
+        The independent variable to be solved.
+    ecc_T_at_x: function
+        Function to solve the eccentricity component along transverse direction.
+    mu: float
+        The gravitational parameter.
+    geometry: tuple
+        A tuple hosting r1_norm, r2_norm, c_norm, dtheta and w_c geometry values.
+    tof12_s: float
+        Desired time of flight to be achieved.
+
+    Notes
+    -----
+    This is equation (14) from Avanzini's report [1].
+
+    """
+
+    # Compute the predicted time of flight at particular ecc_T
+    ecc_T = ecc_T_at_x(x)
+    tof12 = kepler_tof_at_eccT(ecc_T, mu, geometry)
+
+    # Compute the non-dimensional times of flight
+    t_ref = np.sqrt(geometry[0] ** 3 / mu)
+    tau12_s, tau12 = [t / t_ref for t in [tof12_s, tof12]]
+
+    # Compute the values of y
+    y, y_s = [np.log(tau) for tau in [tau12, tau12_s]]
+
+    return y - y_s
+
+
+def coe_at_eccT(ecc_T, r1, r2, sense):
+    """
+    Computes the classical orbita elements at particular value of transverse
+    eccentricity.
+
+    Parameters
+    ----------
+    ecc_T: float
+        Transverse eccentricity.
+    r1: np.array
+        Initial position vecor.
+    r1: np.array
+        Final position vector.
+    sense: bool
+        If True assumes prograde motion, otherwise retrograde is applied.
+
+    Returns
+    -------
+    p:float
+        Orbital parameter or semi-lactus rectum.
+    ecc: float
+        Absolute orbit eccentricity.
+    inc: float
+        Inclination of the orbit.
+    raan: float
+        Right ascension of the ascending node.
+    argp: float
+        Argument of periapsis.
+    nu_1: float
+        True anomaly at the first initial position vector.
+    nu_2: float
+        True anomaly at the first final position vector.
+    """
+
+    # Retrieve auxiliary geometry parameters
+    geometry = get_geometry(r1, r2, sense)
+    r1_norm, r2_norm, c_norm, dtheta, w_c = geometry
+
+    # Orbit parameters as function of actual ecc_T
+    ecc, a, p = eap_from_eccT(ecc_T, geometry)
+    ecc_F, _, _ = get_fundamental_ellipse_properties(r1_norm, r2_norm, c_norm)
+    w = w_at_eccT(ecc_T, ecc_F, w_c)
+    nu_1, nu_2 = get_true_anomalies(w, dtheta)
+
+    # Compute the orbit inclination and RAAN
+    i_r1, i_r2 = [r / r_norm for r, r_norm in zip([r1, r2], [r1_norm, r2_norm])]
+
+    # Compute a normal vector normal to orbit plane with proper sense
+    i_h = get_orbit_normal_vector(r1, r2, sense)
+
+    # Solve for inclination and RAAN
+    inc, raan = get_orbit_inc_and_raan_from_position_vectors(r1, r2, sense)
+
+    # Get a vector in the direction of the line of nodes
+    n = np.cross(np.array([0, 0, 1]), i_h)
+    px = r1.dot(n)
+    py = r1.dot(cross(i_h, n)) / norm(i_h)
+    argp = (np.arctan2(py, px) - nu_1) % (2 * np.pi)
+
+    return p, ecc, inc, raan, argp, nu_1, nu_2
