66218367b234bdab1870153004b7652c73979e34
Jorge Martinez Garrido <contact@jorgemartinez.space>
2023-03-17 11:20:42 +0100

    MAINT: remove plotting sub-package

--- src/lamberthub/plotting/__init__.py
+++
@@ -1,6 +0,0 @@
-""" A sub-package holding a collection of plotting utilities """
-
-from lamberthub.plotting.iterations import IterationsPlotter
-from lamberthub.plotting.time import TPIPlotter, TTCPlotter
-
-__all__ = ["IterationsPlotter", "TPIPlotter", "TTCPlotter"]
